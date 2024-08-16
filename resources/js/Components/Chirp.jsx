bff0070432d063a8522de13451021807eef5faea
amitknows <amitknowsyou@gmail.com>
2024-08-16 22:16:30 +0530

    edit

--- resources/js/Components/Chirp.jsx
+++ resources/js/Components/Chirp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import Dropdown from "@Components/Dropdown";
-import InputError from "@Components/InputError";
-import PrimaryButton from "@Components/PrimaryButton";
+import Dropdown from "./Dropdown";
+import InputError from "./InputError";
+import PrimaryButton from "./PrimaryButton";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { useForm, usePage } from "@inertiajs/react";
@@ -53,7 +53,7 @@ export default function Chirp({ chirp }) {
                              <Dropdown.Trigger>
                                  <button>
                                      <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                         <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                                           <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                      </svg>
                                  </button>
                              </Dropdown.Trigger>
